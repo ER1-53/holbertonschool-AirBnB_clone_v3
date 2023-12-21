878a6b1e4c7461c05bc941b640253d93b1e3e5e0
ER1-53 <cremey.erwan@gmail.com>
2023-12-21 19:33:08 +0100

    optimise

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,7 +3,7 @@
 from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
-from os import environ
+import os
 
 
 app = Flask(__name__)
@@ -19,11 +19,10 @@ def teardown(exception):
 @app.errorhandler(404)
 def page_not_found(exception):
     """404 error"""
-    error = {"error": "Not found"}
-    return jsonify(error), 404
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == '__main__':
-    host = environ.get('HBNB_API_HOST', '0.0.0.0')
-    port = int(environ.get('HBNB_API_PORT', 5000))
+    host = os.environ.get('HBNB_API_HOST', '0.0.0.0')
+    port = int(os.environ.get('HBNB_API_PORT', 5000))
     app.run(host=host, port=port, debug=True)
